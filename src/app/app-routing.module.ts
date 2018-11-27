7df8e15e3075293db4eb307751ebd7f41c1cc97e
Vincent Riva <treast@macbook-pro-de-vincent.home>
2018-11-27 20:48:46 +0100

    Fix guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LoggedInGuard } from './logged-in.guard';
+import { NotLoggedInGuard } from './not-logged-in.guard';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
+    canActivateChild: [LoggedInGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [NotLoggedInGuard]
   }
 ];
 
---
+++ src/app/logged-in.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggedInGuard implements CanActivate {
+  constructor(private authenticationService: AuthenticationService, private router: Router) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authenticationService.isAuthenticated()) { return true; }
+
+    this.router.navigate(['/login']);
+
+    return false;
+  }
+}
